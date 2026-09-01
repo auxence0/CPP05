304a1311c7861b6aff7065173af6b437bedf66e8
auxence0 <sauvagetauxence@gmail.com>
2026-09-01 12:13:57 +0200

    tu veux que je te dises quoi

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asauvage <asauvage@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/31 14:35:53 by asauvage          #+#    #+#             */
-/*   Updated: 2026/09/01 12:06:37 by asauvage         ###   ########.fr       */
+/*   Updated: 2026/09/01 12:13:37 by asauvage         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int	main() {
 
 	try {
 		Bureaucrat	macaron("Meilleur", 1);
+		std::cout << macaron.getName() << " is " << macaron.getGrade() << "\n";
 		macaron.decrement();
 		macaron.increment();
 		macaron.increment();
